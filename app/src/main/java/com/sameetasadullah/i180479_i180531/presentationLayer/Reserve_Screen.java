c969e68c70af1c2e16f36babea4fb000d4fa5f24
DarkDragz <92886415+DarkDragz@users.noreply.github.com>
2021-12-15 06:15:12 +0500

    DarkDragz02

--- app/src/main/java/com/sameetasadullah/i180479_i180531/presentationLayer/Reserve_Screen.java
+++ app/src/main/java/com/sameetasadullah/i180479_i180531/presentationLayer/Reserve_Screen.java
@@ -2,15 +2,93 @@ package com.sameetasadullah.i180479_i180531.presentationLayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Vector;
 
 import com.sameetasadullah.i180479_i180531.R;
+import com.sameetasadullah.i180479_i180531.logicLayer.HRS;
+import com.sameetasadullah.i180479_i180531.logicLayer.Hotel;
 
 public class Reserve_Screen extends AppCompatActivity {
 
+    ImageView backbutton;
+    EditText location,persons,checkinDate,checkoutDate;
+    CheckBox single_room,double_room;
+    RelativeLayout submitButton;
+    HRS hrs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserve_screen);
+
+        backbutton=findViewById(R.id.back_button);
+        location = findViewById(R.id.Location_text);
+        persons = findViewById(R.id.Persons_text);
+        checkinDate = findViewById(R.id.Check_in_date);
+        checkoutDate = findViewById(R.id.Check_out_date);
+        submitButton = findViewById(R.id.submit_button);
+        single_room = findViewById(R.id.Single_box);
+        double_room = findViewById(R.id.Double_box);
+        hrs= HRS.getInstance(Reserve_Screen.this);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Location,Persons,CheckinDate,CheckoutDate;
+                Location = location.getText().toString();
+                Persons = persons.getText().toString();
+                CheckinDate = checkinDate.getText().toString();
+                CheckoutDate = checkoutDate.getText().toString();
+                if(Location.equals("") || Persons.equals("") || CheckinDate.equals("") || CheckoutDate.equals("") ||
+                        (single_room.isChecked()==Boolean.FALSE && double_room.isChecked()==Boolean.FALSE)){
+                    Toast.makeText(Reserve_Screen.this,"Fill All Boxes Correctly.",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Boolean single, doub, both;
+                    both = Boolean.FALSE;
+                    single = Boolean.FALSE;
+                    doub = Boolean.FALSE;
+                    String TypeRoom="";
+                    if (single_room.isChecked()) {
+                        single = Boolean.TRUE;
+                        TypeRoom="Signle";
+                    }
+                    if (double_room.isChecked()) {
+                        doub = Boolean.TRUE;
+                        TypeRoom="Double";
+                    }
+                    if (single && doub) {
+                        both = Boolean.TRUE;
+                        TypeRoom="";
+                    }
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/dd/yyyy");
+                    LocalDate localDate = LocalDate.parse(CheckinDate, formatter);
+
+                    Vector<Hotel> hotels=hrs.getHotels(Location,Persons,localDate,TypeRoom,both);
+                    if (hotels.isEmpty()==Boolean.TRUE){
+                        Toast.makeText(Reserve_Screen.this,"No Hotels Found",Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        Intent intent = new Intent(Reserve_Screen.this, Hotel_Selection.class);
+                        intent.putExtra("Location",Location);
+                        intent.putExtra("Persons",Persons);
+                        intent.putExtra("localDate",localDate);
+                        intent.putExtra("TypeRoom",TypeRoom);
+                        intent.putExtra("both",both);
+                        startActivity(intent);
+                    }
+                }
+            }
+        });
+
+
     }
 }
